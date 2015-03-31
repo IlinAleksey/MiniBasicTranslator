12292c8e57bd241c2ad46412032fdf6419aa763b
agafonovmak <agafonovmak@gmail.com>
2015-03-31 18:15:25 +0300

    test1

--- MiniBasicTranslator/Lexer.h
+++ MiniBasicTranslator/Lexer.h
@@ -95,7 +95,7 @@ class Lexer
 	void B1e(transliterator_token tkn);
 	void C1(transliterator_token tkn);
 	void C1a(transliterator_token tkn);
-	void A1(transliterator_token tkn);
+	void Ñ1b(transliterator_token tkn);
 	void A1(transliterator_token tkn);
 	void A1(transliterator_token tkn);
 	void A1(transliterator_token tkn);
